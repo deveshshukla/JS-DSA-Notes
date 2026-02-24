2e9a584c349d5fb749809b4c22895751c947f756
Devesh Shukla <devshukla291698@gmail.com>
2026-02-24 15:25:55 +0530

    added Stack DS

---
+++ DataStructures/StackDS.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Stack DS is just a framework based on Principal of 'LIFO' : Last In First Out. Implemented using array.
+
+// Stack support operations : push, pop, peek, isEmpty, etc.
+
+// Stack DS Implementation
+class Stack {
+    constructor () {
+        this.Stack = []; // empty arr
+    }
+
+    // push element to stack arr
+    push(element) {
+        this.Stack.push(element);
+    }
+
+    // remove last inserted element from stack arr
+    pop() {
+        if (this.isEmpty()) return 'Stack is empty';
+        return this.Stack.pop();
+    }
+
+    // return latest element of a stack arr
+    peek() {
+        if (this.isEmpty()) return 'Stack is empty';
+        return this.Stack[this.size()-1];
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    size() {
+        return this.Stack.length;
+    }
+};
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(20);
+stack.push(4);
+
+stack.pop();
+
+// console.log(stack);
+// console.log(stack.isEmpty());
+// console.log(stack.peek());
+// console.log(stack.size());
+
+
+// Leet Code Problem using Stack
+var reverseWords = function(s) {
+    let sArr = s.split(' ');
+    let stack = [];
+    let revStr = '';
+
+    for (let i of sArr) stack.push(i);
+
+    while(stack.length) {
+        let check = stack.pop();
+        if (check !== '') revStr += check+" ";
+    }
+
+    return revStr.trim();
+};
+
+let s1 = "the sky is blue";
+let s2 = "  hello world  ";
+let s3 = "a good   example";
+
+console.log(reverseWords(s1));
+console.log(reverseWords(s2));
+console.log(reverseWords(s3));
+
